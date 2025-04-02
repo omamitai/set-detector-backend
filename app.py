e252d9f88e1e32ddee98a6211ed2f831f879e441
omamitai <omermamitai@gmail.com>
2025-04-02 21:27:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app = FastAPI(title="Set Game Detector API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
